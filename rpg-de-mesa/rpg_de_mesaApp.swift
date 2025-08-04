6b9a7b1ef629c7bd1d3354a3050fd4964d87aa40
equipe09 <equipe09.capacitabr@gmail.com>
2025-08-03 23:02:19 -0300

    first commit

---
+++ rpg-de-mesa/rpg_de_mesaApp.swift
@@ -0,0 +1,17 @@
+//
+//  rpg_de_mesaApp.swift
+//  rpg-de-mesa
+//
+//  Created by iredefbmac_25 on 03/08/25.
+//
+
+import SwiftUI
+
+@main
+struct rpg_de_mesaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
